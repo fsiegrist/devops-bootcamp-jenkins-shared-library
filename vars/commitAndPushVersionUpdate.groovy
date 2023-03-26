380a61444ee32e3d04793e6c6e2d3f190539cec8
Felix Siegrist <felix.siegrist@inventage.com>
2023-03-26 15:13:54 +0200

    Add parameter 'branch'

--- vars/commitAndPushVersionUpdate.groovy
+++ vars/commitAndPushVersionUpdate.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def call(String gitRepo, String credentialsId) {
+def call(String gitRepo, String credentialsId, String branch) {
     withCredentials([usernamePassword(credentialsId: "${credentialsId}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh 'git config user.email "jenkins@example.com"'
         sh 'git config user.name "jenkins"'
@@ -7,7 +7,7 @@ def call(String gitRepo, String credentialsId) {
         sh "git remote set-url origin https://${USERNAME}:${PASSWORD}@${gitRepo}"
         sh 'git add .'
         sh 'git commit -m "ci: version bump"'
-        sh 'git push origin HEAD:main'
+        sh "git push origin HEAD:${branch}"
 
         sh 'git config --unset user.email'
         sh 'git config --unset user.name'
